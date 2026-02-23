6f406c4208c6c2296bdb6f97e25f85805267f50a
MirindraAndria <mixandria19@gmail.com>
2026-02-23 19:02:39 +0300

    Commit deploiement
    add corse domain

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: ['http://localhost:4200','http://localhost:8080'], // Remplacez par l'URL de votre frontend
+  origin: ['http://localhost:4200','http://localhost:8080' , 'https://m1p13mean-safidy-mirindra.dev'], // Remplacez par l'URL de votre frontend
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
