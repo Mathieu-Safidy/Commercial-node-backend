858972d9c76d42888a6b2469510b37767a03e57c
MirindraAndria <mixandria19@gmail.com>
2026-02-28 11:02:41 +0300

    Commit
    commit dev

--- base/db.js
+++ base/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/commercial");
+    await mongoose.connect(process.env.MONGO_URL);
+    
     console.log("MongoDB connecté avec Mongoose");
   } catch (error) {
     console.error(error);
